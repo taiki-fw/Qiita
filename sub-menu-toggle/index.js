e9456566515908ffca737ce3c18ee3ab9502684e
taiki-fw <taiki_52@outlook.jp>
2020-05-03 21:44:50 +0900

    Complete

--- sub-menu-toggle/index.js
+++ sub-menu-toggle/index.js
@@ -1,6 +1,10 @@
 const menuBtn = document.getElementById("menu-btn");
 const dropDownMenu = document.getElementsByClassName("dropdown")[0];
+const overlay = document.createElement("div");
+overlay.classList.add("overlay");
 
 menuBtn.addEventListener("click", () => {
   dropDownMenu.classList.toggle("active");
+  if (dropDownMenu.classList.contains("active")) menuBtn.appendChild(overlay);
+  else menuBtn.removeChild(overlay);
 });
